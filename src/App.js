f4c475b15f094f5b1eac82df07125345a6a0b4c5
VJS <minsung_96@naver.com>
2022-03-31 23:52:03 +0900

    Structures #2

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React from 'react'
 import { Switch, Route, Link } from 'react-router-dom'
 import { Layout, Typography, Space } from 'antd'
 
-import { Navbar } from './components'
+import { Navbar, Exchanges, Cryptocurrencies, CryptoDetails, News } from './components'
+import './App.css'
 
 const App = () => {
   return (
@@ -11,7 +12,27 @@ const App = () => {
         <Navbar />
       </div>
       <div className="main">
-
+        <Layout>
+          <div className="routes">
+            <Switch>
+              <Route exact path="/">
+                <Home />
+              </Route>
+              <Route exact path="/eachanges">
+                <Exchanges />
+              </Route>
+              <Route exact path="/cryptocurrencies">
+                <Cryptocurrencies />
+              </Route>
+              <Route exact path="/crypto/:coinId">
+                <CryptoDetails />
+              </Route>
+              <Route exact path="/news">
+                <News />
+              </Route>
+            </Switch>
+          </div>
+        </Layout>
       </div>
       <div className="footer">
 
